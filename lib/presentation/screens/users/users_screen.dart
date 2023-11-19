a204fb2b10c3f6d8e76d5751974bcb509ee02e0e
Jay Cho <jaycho1214@gmail.com>
2023-11-19 21:29:39 +0900

    fix typo from filename

--- lib/presentation/screens/users/users_screen.dart
+++ lib/presentation/screens/users/users_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:prayer/constants/talker.dart';
-import 'package:prayer/presentation/widgets/user/uesr_card.dart';
+import 'package:prayer/presentation/widgets/user/user_card.dart';
 import 'package:prayer/repo/user_repository.dart';
 
 class UsersScreen extends HookWidget {
