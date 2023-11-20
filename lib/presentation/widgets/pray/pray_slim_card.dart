a164f64164f716fa022b8a7d03f2806f39c1292c
Jay Cho <jaycho1214@gmail.com>
2023-11-20 15:59:58 +0900

    divide pray components

---
+++ lib/presentation/widgets/pray/pray_slim_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:prayer/constants/theme.dart';
+import 'package:prayer/model/prayer_pray_model.dart';
+import 'package:prayer/presentation/widgets/user/user_image.dart';
+import 'package:prayer/utils/formatter.dart';
+
+class PraySlimCard extends StatelessWidget {
+  const PraySlimCard({
+    super.key,
+    required this.pray,
+  });
+
+  final PrayerPray pray;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Row(
+        children: [
+          UserProfileImage(
+            profile: pray.user.profile,
+            size: 30,
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Text(
+              '${pray.user.username} has prayed for you',
+              maxLines: 1,
+            ),
+          ),
+          const SizedBox(width: 10),
+          Text(
+            Formatter.fromNow(pray.createdAt),
+            style: TextStyle(
+              color: MyTheme.outline,
+            ),
+          ),
+          const SizedBox(width: 10),
+        ],
+      ),
+    );
+  }
+}
