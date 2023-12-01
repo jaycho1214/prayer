142324841a3c89638417049426336f198663ae2c
Jay Cho <jaycho1214@gmail.com>
2023-12-02 01:21:29 +0900

    track user behaviors

--- lib/constants/mixpanel.dart
+++ lib/constants/mixpanel.dart
@@ -1,3 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:mixpanel_flutter/mixpanel_flutter.dart';
 
 late final Mixpanel mixpanel;
+
+class MixpanelNavigatorObserver extends NavigatorObserver {
+  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    mixpanel.track('Page Visited', properties: {
+      'name': route.settings.name,
+      'arguments': route.settings.arguments,
+    });
+  }
+
+  void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    mixpanel.track('Page Visited', properties: {
+      'name': route.settings.name,
+      'arguments': route.settings.arguments,
+    });
+  }
+}
