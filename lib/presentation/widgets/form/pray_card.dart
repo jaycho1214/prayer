73647fa9a2f38534064d74403e27812bf3f13c5d
Jay Cho <jaycho1214@gmail.com>
2023-12-02 21:06:01 +0900

    fix layout error on android

--- lib/presentation/widgets/form/pray_card.dart
+++ lib/presentation/widgets/form/pray_card.dart
@@ -29,6 +29,7 @@ class PrayCard extends StatelessWidget {
       ),
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             children: [
