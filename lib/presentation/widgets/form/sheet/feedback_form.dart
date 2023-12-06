05247ee9d30c0bbd6865076f09bbad6d0252f9ec
Jay Cho <jaycho1214@gmail.com>
2023-12-06 09:23:07 +0900

    move feedback send algo to feedback form

--- lib/presentation/widgets/form/sheet/feedback_form.dart
+++ lib/presentation/widgets/form/sheet/feedback_form.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:prayer/constants/theme.dart';
 import 'package:prayer/generated/l10n.dart';
 import 'package:prayer/presentation/widgets/button/text_button.dart';
+import 'package:prayer/presentation/widgets/notification_bar.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 class FeedbackForm extends HookWidget {
   const FeedbackForm({super.key});
@@ -23,32 +26,56 @@ class FeedbackForm extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final controller = useTextEditingController();
+    final loading = useState(false);
+    final onTap = useCallback(() async {
+      if (controller.text.trim().isEmpty) {
+        context.pop();
+        return;
+      }
+      loading.value = true;
+      Sentry.captureUserFeedback(SentryUserFeedback(
+        eventId: SentryId.newId(),
+        comments: controller.text,
+        name: FirebaseAuth.instance.currentUser?.uid,
+        email: FirebaseAuth.instance.currentUser?.email,
+      )).then((_) {
+        context.pop();
+        NotificationSnackBar.show(context,
+            message: S.of(context).alertFeedbackSent);
+      }).whenComplete(() {
+        loading.value = false;
+      });
+    }, []);
 
     return Container(
       width: double.infinity,
-      height: MediaQuery.of(context).viewInsets.bottom + 300,
-      padding: EdgeInsets.all(20),
+      padding: EdgeInsets.fromLTRB(
+          20, 20, 20, 20 + MediaQuery.of(context).viewInsets.bottom),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(40),
         color: MyTheme.sheetSurface,
       ),
-      child: Column(
+      child: Wrap(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              PrimaryTextButton(
-                text: S.of(context).cancel,
-                onTap: () => context.pop(null),
-              ),
-              PrimaryTextButton(
-                text: S.of(context).done,
-                onTap: () => context.pop(controller.text),
-              ),
-            ],
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                PrimaryTextButton(
+                  text: S.of(context).cancel,
+                  onTap: () => context.pop(null),
+                ),
+                loading.value
+                    ? CircularProgressIndicator.adaptive()
+                    : PrimaryTextButton(
+                        text: S.of(context).done,
+                        onTap: onTap,
+                      ),
+              ],
+            ),
           ),
-          const SizedBox(height: 10),
           TextField(
             controller: controller,
             autofocus: true,
@@ -60,8 +87,6 @@ class FeedbackForm extends HookWidget {
             minLines: 7,
             maxLines: 7,
           ),
-          const SizedBox(height: 20),
-          const SizedBox(height: 20),
         ],
       ),
     );
