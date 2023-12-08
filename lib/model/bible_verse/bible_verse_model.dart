39c6839933328ad4259216e2db463e08b4bf001e
Jay Cho <jaycho1214@gmail.com>
2023-12-09 02:00:26 +0900

    add bible_verse_model

---
+++ lib/model/bible_verse/bible_verse_model.dart
@@ -0,0 +1,64 @@
+// ignore_for_file: invalid_annotation_target
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:get_it/get_it.dart';
+import 'package:prayer/constants/bible_books.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'bible_verse_model.freezed.dart';
+part 'bible_verse_model.g.dart';
+
+@freezed
+class BibleTranslation with _$BibleTranslation {
+  const factory BibleTranslation({
+    required int id,
+    required String lang,
+    required String abbreviation,
+    required String name,
+  }) = _BibleTranslation;
+
+  static BibleTranslation get KJV => BibleTranslation(
+      id: 1, lang: 'en', abbreviation: 'KJV', name: 'King James Version');
+
+  static BibleTranslation get KRV =>
+      BibleTranslation(id: 2, lang: 'ko', abbreviation: 'KRV', name: '개역한글');
+
+  static List<BibleTranslation> get values =>
+      [BibleTranslation.KJV, BibleTranslation.KRV];
+
+  static BibleTranslation? fromId(int id) {
+    return switch (id) {
+      1 => BibleTranslation.KJV,
+      2 => BibleTranslation.KRV,
+      _ => null,
+    };
+  }
+
+  static BibleTranslation preference() {
+    final pref =
+        GetIt.I<SharedPreferences>().getInt('bible_translations.preference');
+    if (pref == null) {
+      return BibleTranslation.KJV;
+    }
+    return BibleTranslation.fromId(pref) ?? BibleTranslation.KJV;
+  }
+
+  factory BibleTranslation.fromJson(Map<String, Object?> json) =>
+      _$BibleTranslationFromJson(json);
+}
+
+@freezed
+class BibleVerse with _$BibleVerse {
+  const factory BibleVerse({
+    required int id,
+    required BibleBook book,
+    required int chapter,
+    required int verse,
+    @JsonKey(name: 'verse_id') int? verseId,
+    @JsonKey(name: 'translation_id') int? translationId,
+    BibleTranslation? translation,
+    String? value,
+  }) = _BibleVerse;
+
+  factory BibleVerse.fromJson(Map<String, Object?> json) =>
+      _$BibleVerseFromJson(json);
+}
