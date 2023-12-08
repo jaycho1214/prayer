6f204d4fa08d23890c7d6c9d8de8c1c4b9d6f2c6
Jay Cho <jaycho1214@gmail.com>
2023-12-09 02:57:13 +0900

    remmove unnecessary import

--- lib/presentation/widgets/prayer_card.dart
+++ lib/presentation/widgets/prayer_card.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
