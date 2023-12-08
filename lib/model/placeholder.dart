65af00ca112b951fe89b98689ace018ca3c6b2a3
Jay Cho <jaycho1214@gmail.com>
2023-12-09 01:58:56 +0900

    change placeholder

--- lib/model/placeholder.dart
+++ lib/model/placeholder.dart
@@ -1,3 +1,3 @@
 const LoremIpsum =
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
 final PlaceholderDate = DateTime(2000, 1, 1);
