6190706623ba0692d3487d503496fb419ef25bd3
Jay Cho <jaycho1214@gmail.com>
2023-12-27 13:51:23 +0900

    organize widget

--- lib/features/prayer/widgets/prayer_card.dart
+++ lib/features/prayer/widgets/prayer_card.dart
@@ -4,6 +4,7 @@ import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:prayer/constants/theme.dart';
 import 'package:prayer/features/common/widgets/image_list.dart';
+import 'package:prayer/features/prayer/models/prayer_model.dart';
 import 'package:prayer/generated/l10n.dart';
 import 'package:prayer/features/bible/widgets/bible_card_list.dart';
 import 'package:prayer/features/pray/widgets/pray_chip.dart';
@@ -25,6 +26,92 @@ class PrayerCard extends ConsumerWidget {
   final void Function()? onTap;
   final String prayerId;
 
+  Widget _buildGroupLabel(
+    BuildContext context, {
+    required Prayer prayer,
+  }) =>
+      ShrinkingButton(
+        onTap: () => context.push('/groups/${prayer.groupId}'),
+        child: Row(
+          children: [
+            const SizedBox(width: 10),
+            FaIcon(
+              FontAwesomeIcons.userGroupSimple,
+              size: 13,
+              color: MyTheme.placeholderText,
+            ),
+            const SizedBox(width: 5),
+            Text(
+              prayer.group?.name ?? '',
+              style: TextStyle(
+                color: MyTheme.placeholderText,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget _buildCorporateLabel(
+    BuildContext context, {
+    required Prayer prayer,
+  }) =>
+      ShrinkingButton(
+        onTap: () {
+          context.push('/prayers/corporate/${prayer.corporateId}');
+        },
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: FaIcon(
+                FontAwesomeIcons.solidChevronRight,
+                size: 10,
+                color: MyTheme.placeholderText,
+              ),
+            ),
+            Text(
+              prayer.corporate?.title ?? '',
+              style: TextStyle(
+                color: MyTheme.placeholderText,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget _buidlFooterLatestPray(BuildContext context, {Prayer? prayer}) {
+    if (prayer?.pray == null) {
+      return const SizedBox();
+    }
+    return Row(
+      children: [
+        UserProfileImage(
+          profile: prayer!.pray!.profile,
+          size: 30,
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+          child: Text(
+            S.of(context).someoneHasPrayed(prayer.pray!.username),
+            maxLines: 1,
+          ),
+        ),
+        const SizedBox(width: 10),
+        if (prayer.pray?.createdAt != null)
+          Text(
+            Formatter.fromNow(prayer.pray!.createdAt!),
+            style: TextStyle(
+              color: MyTheme.outline,
+            ),
+          ),
+        const SizedBox(width: 10),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final prayer = ref.watch(prayerNotifierProvider(prayerId));
@@ -41,66 +128,17 @@ class PrayerCard extends ConsumerWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              if (prayer.value?.group != null) ...[
+              if (prayer.value?.group != null)
                 Padding(
-                  padding: const EdgeInsets.only(left: 30.0),
+                  padding: const EdgeInsets.only(left: 30.0, bottom: 5),
                   child: Row(
                     children: [
-                      ShrinkingButton(
-                        onTap: () =>
-                            context.push('/groups/${prayer.value?.groupId}'),
-                        child: Row(
-                          children: [
-                            const SizedBox(width: 10),
-                            FaIcon(
-                              FontAwesomeIcons.userGroupSimple,
-                              size: 13,
-                              color: MyTheme.placeholderText,
-                            ),
-                            const SizedBox(width: 5),
-                            Text(
-                              prayer.value?.group?.name ?? '',
-                              style: TextStyle(
-                                color: MyTheme.placeholderText,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+                      _buildGroupLabel(context, prayer: prayer.value!),
                       if (prayer.value?.corporate != null)
-                        ShrinkingButton(
-                          onTap: () {
-                            context.push(
-                                '/prayers/corporate/${prayer.value?.corporateId}');
-                          },
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 5),
-                                child: FaIcon(
-                                  FontAwesomeIcons.solidChevronRight,
-                                  size: 10,
-                                  color: MyTheme.placeholderText,
-                                ),
-                              ),
-                              Text(
-                                prayer.value?.corporate?.title ?? '',
-                                style: TextStyle(
-                                  color: MyTheme.placeholderText,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+                        _buildCorporateLabel(context, prayer: prayer.value!),
                     ],
                   ),
                 ),
-                const SizedBox(height: 5),
-              ],
               Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
@@ -156,34 +194,10 @@ class PrayerCard extends ConsumerWidget {
               Row(
                 children: [
                   Expanded(
-                    child: prayer.value?.pray == null
-                        ? const SizedBox()
-                        : Row(
-                            children: [
-                              UserProfileImage(
-                                profile: prayer.value?.pray?.profile,
-                                size: 30,
-                              ),
-                              const SizedBox(width: 10),
-                              Expanded(
-                                child: Text(
-                                  S.of(context).someoneHasPrayed(
-                                      prayer.value!.pray!.username),
-                                  maxLines: 1,
-                                ),
-                              ),
-                              const SizedBox(width: 10),
-                              if (prayer.value?.pray?.createdAt != null)
-                                Text(
-                                  Formatter.fromNow(
-                                      prayer.value!.pray!.createdAt!),
-                                  style: TextStyle(
-                                    color: MyTheme.outline,
-                                  ),
-                                ),
-                              const SizedBox(width: 10),
-                            ],
-                          ),
+                    child: _buidlFooterLatestPray(
+                      context,
+                      prayer: prayer.value,
+                    ),
                   ),
                   PrayChip(prayerId: prayerId),
                 ],
