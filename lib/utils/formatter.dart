a1a50cfafcb333542556a8b83d04f35643eebd6b
Jay Cho <jaycho1214@gmail.com>
2023-12-30 01:52:14 +0900

    add extension: boldstring

--- lib/utils/formatter.dart
+++ lib/utils/formatter.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class Formatter {
   static String fromNow(DateTime now) {
     final diff = DateTime.now().difference(now);
@@ -33,5 +35,41 @@ class Formatter {
       double result = number / 1000000000;
       return result.toStringAsFixed(1) + 'B';
     }
+  }
+}
+
+extension BoldSubString on Text {
+  Text boldSubString(String target) {
+    final textSpans = List.empty(growable: true);
+    final escapedTarget = RegExp.escape(target);
+    final pattern = RegExp(escapedTarget, caseSensitive: false);
+    final matches = pattern.allMatches(data!);
+
+    int currentIndex = 0;
+    for (final match in matches) {
+      final beforeMatch = data!.substring(currentIndex, match.start);
+      if (beforeMatch.isNotEmpty) {
+        textSpans.add(TextSpan(text: beforeMatch));
+      }
+
+      final matchedText = data!.substring(match.start, match.end);
+      textSpans.add(
+        TextSpan(
+          text: matchedText,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+      );
+
+      currentIndex = match.end;
+    }
+
+    if (currentIndex < data!.length) {
+      final remainingText = data!.substring(currentIndex);
+      textSpans.add(TextSpan(text: remainingText));
+    }
+
+    return Text.rich(
+      TextSpan(children: <TextSpan>[...textSpans]),
+    );
   }
 }
